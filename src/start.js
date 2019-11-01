72459eefc503d8de6da20525c0d6e7e0c2ef42c9
Francisco J. Riveira <frj.riveira@gmail.com>
2019-11-01 13:03:14 +0100

    Part 8 starter code

--- src/start.js
+++ src/start.js
@@ -1,15 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Welcome from "./welcome";
-import { App } from "./app";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Welcome from './welcome';
+import { App } from './app';
+
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import { reducer } from './reducer';
+import { Provider } from 'react-redux';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(reduxPromise)));
 
 let elem;
-const userIsLoggedIn = location.pathname != "/welcome";
+const userIsLoggedIn = location.pathname != '/welcome';
 
 if (!userIsLoggedIn) {
-    elem = <Welcome />;
+	elem = <Welcome />;
 } else {
-    elem = <App />;
+	elem = (
+		<Provider store={store}>
+			<App />;
+		</Provider>
+	);
 }
 
-ReactDOM.render(elem, document.querySelector("main"));
+ReactDOM.render(elem, document.querySelector('main'));
