b786d6fa03e0210e31fff733d2818142e6a88f7c
Francisco J. Riveira <frj.riveira@gmail.com>
2019-11-07 16:15:28 +0100

    Bonus Feature Friends on Profile Pages DONEgit add srcgit add srcgit add src

--- src/friendsof.js
+++ src/friendsof.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
 export default function Friendsof({ visitedId }) {
-	const [ peoples, setPeoples ] = useState([]);
+	const [ peoples, setTheirFriends ] = useState([]);
 	useEffect(
 		() => {
 			(async () => {
@@ -13,7 +13,7 @@ export default function Friendsof({ visitedId }) {
 					(async () => {
 						const { data } = await axios.get(`/api/friendsof/${visitedId}`); // Rewrite this to a working route.
 						console.log('Esta es la data conseguida', data);
-						setPeoples(data);
+						setTheirFriends(data);
 					})();
 				} else {
 					console.log("They are not friends, don't show any div");
@@ -24,22 +24,22 @@ export default function Friendsof({ visitedId }) {
 	);
 
 	return (
-		<div />
-		// <div>
-		// 	<h3>These are the friends from </h3>
-		// 	<ul>
-		// 		{friends.map((user) => (
-		// 			<li style={friends_lis} key={user.first}>
-		// 				<h3>
-		// 					{user.first}
-		// 					{user.last}
-		// 				</h3>
-		// 				<a href={'/user/' + user.id}>
-		// 					<img style={friends_img} src={user.profilepicture} />
-		// 				</a>
-		// 			</li>
-		// 		))}
-		// 	</ul>
-		// </div>
+		<div>
+			<h2>These are their friends</h2>
+			<ul>
+				{peoples.map((people) => (
+					<li key={people.first}>
+						<h3>
+							{people.first}
+							{people.last}
+						</h3>
+						<a href={'/user/' + people.id}>
+							{' '}
+							<img src={people.profilepicture} />
+						</a>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 }
